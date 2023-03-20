6912aae623ee0d5791fe6aec1c7aa074fe4988a4
Nikita Shushpanov <71322742+n1k1tkooo@users.noreply.github.com>
2023-03-20 19:05:57 +0300

    add new file
    add new file

---
+++ AlgebraPolinom/base/THashTableOnList.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#ifndef __HASH_TABLE_ON_LIST_H__
+#define __HASH_TABLE_ON_LIST_H__
+#include "TBaseTable.h"
+
+using namespace std;
+
+struct link
+{
+	Data data;
+	link* pNext;
+};
+
+class THashTableOnList : public TBaseTable
+{
+	link* Tab[1000];
+public:
+	THashTableOnList() {	
+		for (int i = 0; i < 1000; i++) { Tab[i] = nullptr; }
+	} 
+	~THashTableOnList();
+	Data* find(const string key);
+	void insert(const Data& data);
+	void deleteByKey(const string& key); 
+	void print();
+};
+
+#endif
---
+++ AlgebraPolinom/base/THashTableSomething.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#ifndef __HASHTABLIN_H__
+#define __HASHTABLIN_H__
+
+#include "TBaseTable.h"
+
+class THashTableSomething : public TBaseTable 
+{
+    Data* Tab[1000]; 
+public:
+    THashTableSomething() 
+    { 
+        for (int i = 0; i < 1000; i++) 
+        {   
+            Tab[i] = nullptr; 
+        }
+    }
+    ~THashTableSomething();
+    Data* find(const std::string key); 
+    void insert(const Data& data);
+    void deleteByKey(const string& key);
+    void print();
+    int HashIt(string key)
+    {
+        int Hash = 0;
+        for (unsigned int i = 0; i < key.size(); i++) { Hash += (int)key[i]; }
+        Hash *= key.size();
+        return Hash % 1000;
+    }
+};
+
+#endif
